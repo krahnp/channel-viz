468c72c7577ae085bbaef5137f9c74f2fbbbb805
krahnp <peter.krahn@gmail.com>
2013-11-21 19:34:08 -0600

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,11 +12,11 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
+	var defaultKey		= '4iX9H3v8ay4XeyobJTf8R5tK0AMkNujhUaTXF7hh6ZNluCEv', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [584112428], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Turkey Temperature Tracker', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
+		dataInterval	= 3600, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
 
