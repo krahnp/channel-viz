9f10a83449a19ece925821936cf25ddd91ffc310
krahnp <peter.krahn@gmail.com>
2013-11-24 15:29:55 -0600

    Cleaned up code.  Adjusted default duration

--- follows.js
+++ follows.js
@@ -12,11 +12,11 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= 'A5XzvX8WkoY3CLGZXc5SdzzdoUIIY2EIdwQzqnkMXTKYAxkz', // Unique master Xively API key to be used as a default
+	var defaultKey		= 'OEPYda9cMSiTdAHYIgCrM0N5GqMbBCxTOQ78Jg18aLDekJfp', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [584112428], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'Turkey Temperature', // Replaces Xively logo in the header
-		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= 3600, // Default interval for data to be displayed (in seconds)
+		dataDuration	= '6hours', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataInterval	= 60, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
 
