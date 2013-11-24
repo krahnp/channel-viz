09fec0b242f5d9e987f2ecd7f54f2743cd044c42
krahnp <peter.krahn@gmail.com>
2013-11-24 14:57:50 -0600

    further key info

--- follows.js
+++ follows.js
@@ -14,7 +14,7 @@
 
 	var defaultKey		= 'A5XzvX8WkoY3CLGZXc5SdzzdoUIIY2EIdwQzqnkMXTKYAxkz', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [584112428], // Comma separated array of Xively Feed ID numbers
-		applicationName	= 'Turkey Temperature Tracker', // Replaces Xively logo in the header
+		applicationName	= 'Turkey Temperature', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 3600, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
